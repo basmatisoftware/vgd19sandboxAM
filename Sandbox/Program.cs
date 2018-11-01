6cf00c38ebf226ec9b363a56fcf12894ea4aee59
Bassett James B <BassettJB@mukilteo.wednet.edu>
2018-10-31 21:30:41 -0700

    Demo Push

---
+++ Sandbox/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int x = 7;
+            }
+            {
+                int x = 3;
+            }
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
